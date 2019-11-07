25d27e73b69e4a765c0cd845dc0e66e083582a33
Jasmine Hanifa Mounir <jasmine.mounir@gmail.com>
2019-11-07 17:13:22 +0700

    make muaic player header

---
+++ VTune/Controller/MusicPlayer/MusicPlayerLyricHeader.swift
@@ -0,0 +1,16 @@
+//
+//  MusicPlayerLyricHeader.swift
+//  VTune
+//
+//  Created by Jasmine Hanifa Mounir on 07/11/19.
+//  Copyright © 2019 Jasmine Hanifa Mounir. All rights reserved.
+//
+
+import UIKit
+
+class MusicPlayerLyricHeader: UIView {
+
+    @IBOutlet weak var albumImage: UIImageView!
+    @IBOutlet weak var nowPlayingSongTitle: UILabel!
+    @IBOutlet weak var nowPlayingSinger: UILabel!
+}
