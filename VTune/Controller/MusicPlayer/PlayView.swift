829533b773bdd562da2f26a81558e9ed73086af3
Stefani <stefani.dewi@binus.ac.id>
2019-11-08 14:49:26 +0700

    Play View dan Lirik  View Ready

---
+++ VTune/Controller/MusicPlayer/PlayView.swift
@@ -0,0 +1,48 @@
+//
+//  PlayView.swift
+//  VTune
+//
+//  Created by Stefani Kurnia Permata Dewi on 07/11/19.
+//  Copyright © 2019 Jasmine Hanifa Mounir. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class playView: UIView {
+    
+    @IBOutlet weak var timeSlider: UISlider!
+    @IBOutlet weak var timeDuration: UILabel!
+    @IBOutlet weak var volumeSlider: UISlider!
+    @IBOutlet weak var btnPlay: UIButton!
+    @IBOutlet weak var btnPrevious: UIButton!
+    @IBOutlet weak var btnNext: UIButton!
+    @IBOutlet weak var btnShuffle: UIButton!
+    @IBOutlet weak var btnRepeat: UIButton!
+    
+
+    
+    @IBAction func btnPlay(_ sender: Any) {
+    }
+    
+    @IBAction func btnNext(_ sender: Any) {
+    }
+    
+    @IBAction func btnPrevious(_ sender: Any) {
+    }
+    
+    
+    @IBAction func btnRepeat(_ sender: Any) {
+    }
+    
+    
+    @IBAction func btnShuffle(_ sender: Any) {
+    }
+    
+    @IBAction func btnVibrate(_ sender: Any) {
+        }
+
+    @IBAction func btnLirik(_ sender: Any) {
+    }
+}
+   
